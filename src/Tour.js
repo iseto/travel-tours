654b1f992a271c2a77497dd1c79a871148190db1
Ignatius Seto <ignatius.seto@accenture.com>
2022-07-07 19:44:21 +0200

    add functionalities in Tour component

--- src/Tour.js
+++ src/Tour.js
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
-  return (
-    <article className="single-tour">
-      <img src={image} alt={name} />
+    const [readMore, setReadMore] = useState(false);
 
-      <footer>
-        <div className="tour-info">
-          <h4>{name}</h4>
-          <h4 className="tour-price">€ {price}</h4>
-        </div>
-        <p>{info}</p>
-        <button className="delete-btn">Not Interested</button>
-      </footer>
-    </article>
-  );
+    return (
+      <article className="single-tour">
+        <img src={image} alt={name} />
+
+        <footer>
+          <div className="tour-info">
+            <h4>{name}</h4>
+            <h4 className="tour-price">€ {price}</h4>
+          </div>
+          <p>
+            {readMore ? info : `${info.substring(0, 200)}...`}
+            <button onClick={() => setReadMore(!readMore)}>
+              {readMore ? "show less" : "read more"}
+            </button>
+          </p>
+
+          <button className="delete-btn">Not Interested</button>
+        </footer>
+      </article>
+    );
 };
 
 export default Tour;
