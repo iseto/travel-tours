cf426867ca4e1cf0254a3f3e47e6f0b799b4793c
Ignatius Seto <ignatius.seto@accenture.com>
2022-07-07 19:53:30 +0200

    Finish adding components and functionalities

--- src/Tour.js
+++ src/Tour.js
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, image, info, price, name }) => {
-    const [readMore, setReadMore] = useState(false);
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
 
-    return (
-      <article className="single-tour">
-        <img src={image} alt={name} />
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
 
-        <footer>
-          <div className="tour-info">
-            <h4>{name}</h4>
-            <h4 className="tour-price">€ {price}</h4>
-          </div>
-          <p>
-            {readMore ? info : `${info.substring(0, 200)}...`}
-            <button onClick={() => setReadMore(!readMore)}>
-              {readMore ? "show less" : "read more"}
-            </button>
-          </p>
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">€ {price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
 
-          <button className="delete-btn">Not Interested</button>
-        </footer>
-      </article>
-    );
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
